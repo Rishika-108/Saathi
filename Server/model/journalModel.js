bdba121586d598cbb6750f10671a8b05556ce535
Rishika-108 <rishikathakur334@gmail.com>
2025-10-08 19:52:22 +0530

    Setup of - Journal Model. Controller, Routes and AI Engine

---
+++ Server/model/journalModel.js
@@ -0,0 +1,31 @@
+import mongoose, { Schema } from 'mongoose'
+
+const TriggerSchema = new mongoose.Schema({
+    triggerID : {type: Schema.Types.ObjectId},
+    triggerPoint : {type : String, required: true},
+    triggerScene : {type: String, required: true},
+})
+
+const SolutionSchema = new mongoose.Schema({
+    triggerID: {type: String, required: true},
+    // suggestionText : {type: String, required: true},
+    steps: [
+    {
+      stepNumber: { type: Number, required: true },
+      description: { type: String, required: true }
+    }
+  ],
+    status : {type: String, enum: ['pending', 'completed', 'dismissed'], default: 'pending'}
+})
+
+const JournalEntrySchema = new mongoose.Schema({
+    userID : {type: Schema.Types.ObjectId, required: true, ref: 'user'},
+    text : {type: String, required: true},
+    createdAt: {type: Date, default: Date.now},
+    triggers: [TriggerSchema],
+    solutions: [SolutionSchema]
+})
+
+const JournalModel = mongoose.model.journal || mongoose.model ('journal', JournalEntrySchema)
+
+export default JournalModel
